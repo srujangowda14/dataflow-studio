4d7fbe8fcdae315b86e5f79b3df5607c85123a40
srujangowda14 <srujugowda456@gmail.com>
2025-12-02 01:50:00 -0800

    structure change

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import DataVizPortfolio from '../data-viz-portfolio.jsx'
+import DataVizPortfolio from './data-viz-portfolio.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
