550d73744b487eeacb8075ca15e4c7c1fcd84805
srujangowda14 <srujugowda456@gmail.com>
2025-12-01 20:09:01 -0800

    Corrected the path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dataviz-portfolio/',  // Add this line!
+  base: '/dataflow-studio',  
   server: {
     port: 3000,
     open: true
