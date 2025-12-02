306d812f7c76a8875f927739e029eacc2f665135
srujangowda14 <srujugowda456@gmail.com>
2025-12-01 20:04:49 -0800

    Deployment to github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/dataviz-portfolio/',  // Add this line!
   server: {
     port: 3000,
     open: true
